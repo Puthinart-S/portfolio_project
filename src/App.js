2a711325c5aa694a65875146b68cd4bf9cc84f14
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-24 15:23:15 +0700

    first create

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+const navCSS = {
+  color : 'red',
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={navCSS}>MyPortFolio</h1>
+      {/* <h2></h2> */}
+    </div>
+  );
+}
+
+export default App;
