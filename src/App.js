af2585ff19f2c32b70c75227f4e5696a7208ef38
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-24 16:32:41 +0700

    settings sth.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React, { Component, Fragment } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const navCSS = {
-  color : 'red',
+  color: 'red',
 }
 
-function App() {
-  return (
-    <div className="App">
-      <h1 style={navCSS}>MyPortFolio</h1>
-      {/* <h2></h2> */}
-      <h3>test deploy</h3>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="App">
+          <h1 style={navCSS}>MyPortFolio</h1>
+          {/* <h2></h2> */}
+          <h3>test deploy</h3>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
