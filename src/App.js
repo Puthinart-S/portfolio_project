dcb6044d3970846e2c997b7647204fcd60d46599
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-24 18:40:46 +0700

    try add navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component, Fragment } from 'react'
-import './App.css';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Profile } from './Profile';
+import { MyProject } from './MyProject';
+import { NoMatch } from './NoMatch';
+import { Layout } from './Components/Layout';
+import { NaviBar } from './Components/NaviBar';
 
-
-const navCSS = {
-  color: 'red',
-}
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="App">
-          <h1 style={navCSS}>MyPortFolio</h1>
-          {/* <h2></h2> */}
-          <h3>test deploy</h3>
-        </div>
+        <NaviBar/>
+        <Layout>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Profile} />
+              <Route path="/MyProject" component={MyProject} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Layout>
       </React.Fragment>
     );
   }
